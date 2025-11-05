f97f456f9cf0019a27f0e55cf4c2b64841c98116
ContadXIV <conrad_cabrera@ymail.com>
2025-11-05 13:59:00 +0000

    Aggiornato a MATERIE PRIME

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('cucina-spave-v1').then(c => c.addAll([
+  e.waitUntil(caches.open('cucina-spave-v2').then(c => c.addAll([
     './','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'
   ])));
 });
